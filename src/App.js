0a69ed474d72c2198590220aeb1466bd2c004792
Zyzz294 <hamzaozgezici294@gmail.com>
2024-04-06 17:49:51 +0500

    Adding SEO friendly tags

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <head>
+      <meta name='pixel perfect solutions' content='pixel perfect solutions'/>
+      <title>Pixel Perfect Solutions</title>
+     </head>
+     <body>
+      <h1>Pixel Perfect Solutions</h1>
+     </body>
+
+     <footer>
+      <p>Pixel Perfect Solutions 2024</p>
+     </footer>
+    </div>
+  );
+}
+
+export default App;
